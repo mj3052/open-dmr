7e6c48696366b072c324a031690f80f5df4bec70
Mathias Jensen <mj@8v.dk>
2021-11-30 23:38:31 +0100

    JSON mapping

--- app/model.go
+++ app/model.go
@@ -1,41 +1,41 @@
 package main
 
 type VehicleBaseInfo struct {
-	VIN    string `xml:"KoeretoejOplysningStelNummer"`
-	Status string `xml:"KoeretoejOplysningStatus"`
+	VIN    string `xml:"KoeretoejOplysningStelNummer" json:"vin"`
+	Status string `xml:"KoeretoejOplysningStatus" json:"status"`
 
-	FirstRegistered string `xml:"KoeretoejOplysningFoersteRegistreringDato"`
+	FirstRegistered string `xml:"KoeretoejOplysningFoersteRegistreringDato" json:"first_registered"`
 
 	// Weight + Pull
-	WeightSelf  int `xml:"KoeretoejOplysningEgenVaegt"`
-	WeightTotal int `xml:"KoeretoejOplysningTekniskTotalVaegt"`
-	WeightMax   int `xml:"KoeretoejOplysningTotalVaegt"`
-	Axes        int `xml:"KoeretoejOplysningAkselAntal"`
+	WeightSelf  int `xml:"KoeretoejOplysningEgenVaegt" json:"weight_self"`
+	WeightTotal int `xml:"KoeretoejOplysningTekniskTotalVaegt" json:"weight_total"`
+	WeightMax   int `xml:"KoeretoejOplysningTotalVaegt" json:"weight_max"`
+	Axes        int `xml:"KoeretoejOplysningAkselAntal" json:"axes"`
 
 	// Brand, model, color
-	Brand   string `xml:"KoeretoejBetegnelseStruktur>KoeretoejMaerkeTypeNavn"`
-	Model   string `xml:"KoeretoejBetegnelseStruktur>Model>KoeretoejModelTypeNavn"`
-	Variant string `xml:"KoeretoejBetegnelseStruktur>Variant>KoeretoejVariantTypeNavn"`
-	Type    string `xml:"KoeretoejBetegnelseStruktur>Type>KoeretoejTypeTypeNavn"`
-	Color   string `xml:"KoeretoejFarveStruktur>FarveTypeStruktur>FarveTypeNavn"`
+	Brand   string `xml:"KoeretoejBetegnelseStruktur>KoeretoejMaerkeTypeNavn" json:"brand"`
+	Model   string `xml:"KoeretoejBetegnelseStruktur>Model>KoeretoejModelTypeNavn" json:"model"`
+	Variant string `xml:"KoeretoejBetegnelseStruktur>Variant>KoeretoejVariantTypeNavn" json:"variant"`
+	Type    string `xml:"KoeretoejBetegnelseStruktur>Type>KoeretoejTypeTypeNavn" json:"type"`
+	Color   string `xml:"KoeretoejFarveStruktur>FarveTypeStruktur>FarveTypeNavn" json:"color"`
 
 	// Engine
-	EngineMileage    string  `xml:"KoeretoejMotorStruktur>KoeretoejMotorKmPerLiter"`
-	EngineKilometers float32 `xml:"KoeretoejMotorStruktur>KoeretoejMotorKilometerstand"`
-	EngineFuel       string  `xml:"KoeretoejMotorStruktur>DrivkraftTypeNavn"`
-	EngineCylinders  int     `xml:"KoeretoejMotorStruktur>KoeretoejMotorCylinderAntal"`
-	EngineVolume     float32 `xml:"KoeretoejMotorStruktur>KoeretoejMotorSlagVolumen"`
-	EngineEffect     float32 `xml:"KoeretoejMotorStruktur>KoeretoejMotorStoersteEffekt"`
+	EngineMileage    string  `xml:"KoeretoejMotorStruktur>KoeretoejMotorKmPerLiter" json:"engine_mileage"`
+	EngineKilometers float32 `xml:"KoeretoejMotorStruktur>KoeretoejMotorKilometerstand" json:"engine_kilometers"`
+	EngineFuel       string  `xml:"KoeretoejMotorStruktur>DrivkraftTypeNavn" json:"engine_fuel"`
+	EngineCylinders  int     `xml:"KoeretoejMotorStruktur>KoeretoejMotorCylinderAntal" json:"engine_cylinders"`
+	EngineVolume     float32 `xml:"KoeretoejMotorStruktur>KoeretoejMotorSlagVolumen" json:"engine_volume"`
+	EngineEffect     float32 `xml:"KoeretoejMotorStruktur>KoeretoejMotorStoersteEffekt" json:"engine_effect"`
 
-	ChassisType string `xml:"KarrosseriTypeStruktur>KarrosseriTypeNavn"`
+	ChassisType string `xml:"KarrosseriTypeStruktur>KarrosseriTypeNavn" json:"chassis_type"`
 }
 
 type Vehicle struct {
-	Plate       string `xml:"RegistreringNummerNummer" gorm:"primaryKey"`
-	VehicleType string `xml:"KoeretoejArtNavn"`
+	Plate       string `xml:"RegistreringNummerNummer" gorm:"primaryKey" json:"plate"`
+	VehicleType string `xml:"KoeretoejArtNavn" json:"vehicle_type"`
 
-	BaseInfo VehicleBaseInfo `xml:"KoeretoejOplysningGrundStruktur" gorm:"embedded"`
+	BaseInfo VehicleBaseInfo `xml:"KoeretoejOplysningGrundStruktur" gorm:"embedded" json:"info"`
 
-	End   string `xml:"RegistreringNummerUdloebDato"`
-	Usage string `xml:"KoeretoejAnvendelseStruktur>KoeretoejAnvendelseNavn"`
+	End   string `xml:"RegistreringNummerUdloebDato" json:"expiry_date"`
+	Usage string `xml:"KoeretoejAnvendelseStruktur>KoeretoejAnvendelseNavn" json:"usage"`
 }
