f8782dd97cbd177c2518f25677c58aafa6ba5fd4
Mathias Jensen <mj@8v.dk>
2021-11-30 10:40:50 +0100

    Experiment

--- app/storage.go
+++ app/storage.go
@@ -29,8 +29,8 @@ func (vdb *VehicleDB) LoadData(filename string) {
 	p := ParseDataFile(filename, "Statistik", true)
 
 	// Initialize buffer
-	batchSize := 5000
-	buffer := make([]Vehicle, 0, batchSize)
+	// batchSize := 5000
+	// buffer := make([]Vehicle, 0, batchSize)
 
 	i := 0
 	for vehicle := range p {
@@ -39,19 +39,24 @@ func (vdb *VehicleDB) LoadData(filename string) {
 		if vehicle.Plate == "" || vehicle.BaseInfo.Status == "Afmeldt" {
 			continue
 		}
+		vdb.db.Create(vehicle)
 
-		buffer = append(buffer, vehicle)
+		// buffer = append(buffer, vehicle)
 		i++
 
-		// Flush buffer every time it's full
-		if i%batchSize == 0 {
-			// vdb.processBatch(buffer)
-			vdb.db.CreateInBatches(buffer, 1000)
-
-			buffer = buffer[:0] // Slice to 0 to reuse memory for next batch
+		if i%1000 == 0 {
 			fmt.Printf("%d items inserted\n", i)
 		}
+
+		// // Flush buffer every time it's full
+		// if i%batchSize == 0 {
+		// 	// vdb.processBatch(buffer)
+		// 	vdb.db.CreateInBatches(buffer, 1000)
+
+		// 	buffer = buffer[:0] // Slice to 0 to reuse memory for next batch
+		// 	fmt.Printf("%d items inserted\n", i)
+		// }
 	}
 	// Flush buffer on end
-	vdb.db.CreateInBatches(buffer, 1000)
+	// vdb.db.CreateInBatches(buffer, 1000)
 }
